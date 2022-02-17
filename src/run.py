d324e5fba026cb421bed375f75f8d6d4a2d2fe4d
Daniel Ma <dma1@DNa1ca8c9.SUNet>
2022-02-17 02:52:15 -0800

    synthesizer

--- src/run.py
+++ src/run.py
@@ -91,7 +91,7 @@ if args.function == 'pretrain':
         lr_decay=True,
         warmup_tokens=512*20,
         final_tokens=200*len(pretrain_dataset)*block_size,
-        num_workers=4 
+        num_workers=4
     )
     trainer = trainer.Trainer(model, pretrain_dataset, None, tconf)
     trainer.train()
